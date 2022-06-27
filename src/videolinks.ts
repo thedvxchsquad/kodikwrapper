1eba64a18ab6f1bfc16e461924264830e128fa1c
th3egdvach <naer200469@gmail.com>
2022-06-27 16:26:54 +0300

    fix videolinks b64 decode

--- src/videolinks.ts
+++ src/videolinks.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ArrayAsObject } from "./utils";
 
-export const linkPlayerRegex = /^(?<protocol>http[s]?:|)\/\/(?<host>[a-z0-9]+\.[a-z]+)\/(?<type>serial|video)\/(?<id>\d+)\/(?<hash>[0-9a-z]+)\/(?<quality>\d+p)$/;
+export const linkPlayerRegex = /^(?<protocol>http[s]?:|)\/\/(?<host>[a-z0-9]+\.[a-z]+)\/(?<type>video|seria|season)\/(?<id>\d+)\/(?<hash>[0-9a-z]+)\/(?<quality>\d+p)$/;
 
 export interface ParsedLink {
   type: string;
@@ -20,9 +20,9 @@ export interface Source {
   type: string;
 }
 export type VideoLinksTypes = Record<
-  "links", 
+  "links",
   Record<
-    "720" | "480" | "360" | "240" | string, 
+    "720" | "480" | "360" | "240" | string,
     Source[]
   >
 >
@@ -65,16 +65,16 @@ export async function parseLink(
     type: ""
   };
   const linkParams = linkPlayerRegex.exec(params.link)!;
-  if(!linkParams.groups) throw new Error(`cannot get "groups" from "linkParams"`);
+  if (!linkParams.groups) throw new Error(`cannot get "groups" from "linkParams"`);
   Object.assign(data, linkParams.groups);
-  if(params.extended) {
+  if (params.extended) {
     try {
       const page = await axios.get<string>(`${linkParams.groups.protocol.length === 0 ? "https:" : ""}${params.link}`);
       const pageMatch = page.data.match(/iframe\.src\s*=\s*"(?<url>[^"]+)";/);
-      if(!pageMatch?.groups?.url) throw "ывзаыхвахывха че за";
+      if (!pageMatch?.groups?.url) throw "ывзаыхвахывха че за";
       const extFields = new URL("https:" + pageMatch.groups.url);
-      const obj = Array.from(extFields.searchParams).reduce((p, [k,v]) => (p[k]=v, p), {} as Record<string, string>);
-      return {...data, ...obj};
+      const obj = Array.from(extFields.searchParams).reduce((p, [k, v]) => (p[k] = v, p), {} as Record<string, string>);
+      return { ...data, ...obj };
     } catch (error) {
       throw new ParseError("cannot get extended fields")
     };
@@ -98,10 +98,10 @@ export interface GetLinksParams {
 }
 
 export async function getLinks(params: GetLinksParams): Promise<VideoLinksParsed> {
-  
+
   params.extended = params.extended || false;
   params.host = "host" in params ? params.host : "https://kodik.cc"
-  
+
   const parsedLink = await parseLink({
     ...params,
     extended: true
@@ -109,12 +109,22 @@ export async function getLinks(params: GetLinksParams): Promise<VideoLinksParsed
   const rawVideoLinksResponse = await axios.post<VideoLinksTypes>("https://aniqit.com/gvi", null, {
     params: parsedLink
   });
-  const videoLinks = Object.entries(rawVideoLinksResponse.data.links).reduce((response, [q, src]) => {
-    response.links[q] = src
-    return response
-  }, {links: {}} as VideoLinksTypes);
+  const videoLinks = Object
+    .entries(rawVideoLinksResponse.data.links)
+    .reduce(
+      (response, qualityVariant) => (
+        response[qualityVariant[0]] = qualityVariant[1]
+          .map(
+            sourceVariant => (
+              {
+                ...sourceVariant,
+                src: Buffer.from(sourceVariant.src.split("").reverse().join(""), "base64").toString()
+              }
+            )
+          ),
+        response), {} as VideoLinksTypes["links"]);
   return {
-    ...videoLinks,
-    ...(params.extended ? {params} : {})
+    links: videoLinks,
+    ...(params.extended ? { params } : {})
   }
 }

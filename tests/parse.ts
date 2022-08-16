24e253f1de66a9188659a735ce897a99542c34fd
th3egdvach <naer200469@gmail.com>
2022-08-16 18:30:29 +0300

    update parse.ts test

--- tests/parse.ts
+++ tests/parse.ts
@@ -1,9 +1,9 @@
 import { expect } from "chai";
-import { parseLink } from "../src/index";
+import { VideoLinks } from "../src/videolinks";
 
 describe("parse.ts", () => {
   it("Parse link correctly", async () => {
-    const parsedLink = await parseLink({ link: "//kodik.cc/video/87051/57c572172a2ccfb23234cdaf7174b20d/720p" });
+    const parsedLink = await VideoLinks.parseLink({ link: "//kodik.cc/video/87051/57c572172a2ccfb23234cdaf7174b20d/720p", extended: true });
     expect(parsedLink.type).string("video", "param type");
     expect(parsedLink.quality).string("720", "param quality");
     expect(parsedLink.id).string("87051", "param id");
